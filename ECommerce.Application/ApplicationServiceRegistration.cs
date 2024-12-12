476b8fa0a25337a21d5ba606f4ab81d66eb75697
esryllmz <63209900+esryllmz@users.noreply.github.com>
2024-12-13 01:03:29 +0300

    Add files via upload

---
+++ ECommerce.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using Core.Application.Pipelines.Authorization;
+using Core.Application.Pipelines.Login;
+using Core.Application.Pipelines.Performance;
+using Core.Application.Pipelines.Validation;
+using ECommerce.Application.Features.Categories.Rules;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ECommerce.Application;
+
+public static class ApplicationServiceRegistration
+{
+  public static IServiceCollection AddApplicationServiceDependencies(this IServiceCollection services)
+  {
+    services.AddScoped<CategoryBusinessRules>();
+
+    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+    services.AddValidatorsFromAssemblies([Assembly.GetExecutingAssembly()]);
+    services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+    services.AddMediatR(con => {
+      con.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+      con.AddOpenBehavior(typeof(RequestValidationBehavior<,>));
+      con.AddOpenBehavior(typeof(AuthorizationBehavior<,>));
+      con.AddOpenBehavior(typeof(LoginBehavior<,>));
+    });
+
+    return services;
+  }
+}
---
+++ ECommerce.Application/Features/Categories/Commands/Create/CategoryAddCommand.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Core.Application.Pipelines.Login;
+using ECommerce.Application.Features.Categories.Rules;
+using ECommerce.Application.Services.Repositories;
+using ECommerce.Domain.Entities;
+using MediatR;
+
+namespace ECommerce.Application.Features.Categories.Commands.Create;
+
+public sealed class CategoryAddCommand : IRequest<CategoryAddedResponseDto>, ILoginRequest
+{
+  public string Name { get; set; }
+
+  public sealed class CategoryAddCommandHandler(IMapper _mapper, ICategoryRepository _categoryRepository, CategoryBusinessRules _businessRules) : IRequestHandler<CategoryAddCommand, CategoryAddedResponseDto>
+  {
+    public async Task<CategoryAddedResponseDto> Handle(CategoryAddCommand request, CancellationToken cancellationToken)
+    {
+      await _businessRules.CategoryNameMustBeUniqueAsync(request.Name, cancellationToken);
+
+      Category category = _mapper.Map<Category>(request);
+
+      Category addedCategory = await _categoryRepository.AddAsync(category);
+
+      CategoryAddedResponseDto response = _mapper.Map<CategoryAddedResponseDto>(addedCategory);
+
+      return response;
+    }
+  }
+}
